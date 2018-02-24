996c3219493fa961e5228aff2dcbb72e24f42fce
canopus <tsungtingdu@gmail.com>
2018-02-24 19:56:43 +0800

    - refactoring new and edit page - install gem carrierwave - add uploading photo feature

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -54,7 +54,7 @@ class Admin::RestaurantsController < ApplicationController
     end
 
     def restaurant_params
-      params.require(:restaurant).permit(:name, :tel, :address, :opening_hours,  :description)
+      params.require(:restaurant).permit(:name, :tel, :address, :opening_hours, :description, :image)
     end
 
     def set_restaurant
