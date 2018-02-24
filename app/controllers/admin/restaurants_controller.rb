229261b4b29df25073c1873ddd198247cd497e60
canopus <tsungtingdu@gmail.com>
2018-02-24 16:31:33 +0800

    set up admin authentication

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,7 +1,17 @@
 class Admin::RestaurantsController < ApplicationController
+    before_action :authenticate_user!
+    before_action :authenticate_admin
 
   def index
-    
   end
-  
+
+  private
+
+    def authenticate_admin
+      unless current_user.admin?
+        flash[:alert] = "NOT ALLOW!"
+        redirect_to root_path
+      end  
+    end
+
 end
