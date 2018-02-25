462b39a1cee5fdae726699a170e55b04820563ab
canopus <tsungtingdu@gmail.com>
2018-02-25 23:56:36 +0800

    set up seed data for default categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Caregory
+
+Category.destroy_all
+
+category_list = [
+  {name: "Taiwanese Food"},
+  {name: "Chinese Food"},
+  {name: "Japanese Food"},
+  {name: "Korean Food"},
+  {name: "Singaporean Food"},
+  {name: "Western Food"},
+  {name: "Veg Food"},      
+]
+
+category_list.each do |category|
+  Category.create(name: category[:name])
+end  
+
+puts "Category created"
