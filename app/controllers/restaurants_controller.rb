a5b26ee77c1b24076cce6d4d8fbee6b47f2ce459
canopus <tsungtingdu@gmail.com>
2018-03-07 01:00:04 +0800

    add show page for public users

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,11 @@
 class RestaurantsController < ApplicationController
 
   def index
-    @restaurants = Restaurant.page(params[:page]).per(12)
+    @restaurants = Restaurant.page(params[:page]).per(9)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end
