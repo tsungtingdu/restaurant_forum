617e65c5d84b2a1eb8841599d675406997bf91c9
canopus <tsungtingdu@gmail.com>
2018-03-18 11:00:08 +0800

    setup the validation so that user can't edit other user's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,36 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user
 
   def show
-    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    flash[:notice] = "User profile was successfully updated"
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :intro, :avatar)
+  end
+
+  def set_user
+    begin
+      if current_user == User.find(params[:id])
+        @user = User.find(params[:id])
+      else
+        flash[:alert] = "NOT ALLOW!"
+        redirect_to user_path(current_user)
+      end 
+    rescue
+      redirect_to user_path(current_user)
+    end   
   end
 
 end
