a00dc7e2718f2e492e59807ad30536191bbd6d35
canopus <tsungtingdu@gmail.com>
2018-03-27 00:40:12 +0800

    set restaurant api v1 resources

--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :photos, only: [:index, :create, :show, :update, :destroy]
+      resources :restaurants, only: [:index, :create, :show, :update, :destroy]
     end
   end
 
