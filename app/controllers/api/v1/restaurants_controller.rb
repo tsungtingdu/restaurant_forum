c68c8df2d4d8cdf0a2747f8a20c0f1b11039a239
canopus <tsungtingdu@gmail.com>
2018-03-27 08:53:00 +0800

    add create action for restaurant api

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -5,4 +5,24 @@ class Api::V1::RestaurantsController < ApiController
     render json: @restaurants
   end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      render json: {
+        message: "New restaurant has been created successfully",
+        result: @restaurant
+      }
+    else
+      render json: {
+        message: "Error",
+        errors: @restaurant.errors
+      }
+    end 
+  end
+
+  private
+
+  def restaurant_params
+    params.permit(:name, :tel, :address, :opening_hours, :description)
+  end
 end
